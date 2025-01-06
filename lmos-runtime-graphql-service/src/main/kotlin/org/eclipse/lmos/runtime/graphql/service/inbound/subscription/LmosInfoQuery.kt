4fcb68e4e7345302ae7061c61124b8fab3e14334
bharatnpti <bharatnpti@gmail.com>
2025-01-06 18:30:22 +0530

    GraphQL

---
+++ lmos-runtime-graphql-service/src/main/kotlin/org/eclipse/lmos/runtime/graphql/service/inbound/subscription/LmosInfoQuery.kt
@@ -0,0 +1,17 @@
+package org.eclipse.lmos.runtime.graphql.service.inbound.subscription
+
+import com.expediagroup.graphql.generator.annotations.GraphQLDescription
+import com.expediagroup.graphql.server.operations.Query
+import kotlinx.coroutines.coroutineScope
+import org.eclipse.lmos.runtime.core.service.outbound.AgentRegistryService
+import org.springframework.stereotype.Component
+
+@Component
+class LmosInfoQuery(private val agentRegistryService: AgentRegistryService) : Query {
+
+    @GraphQLDescription("Returns a greeting message")
+    suspend fun getAvailableAgents(tenantId: String, channelId: String) = coroutineScope {
+        agentRegistryService.getRoutingInformation(tenantId, channelId).agentList
+    }
+
+}
