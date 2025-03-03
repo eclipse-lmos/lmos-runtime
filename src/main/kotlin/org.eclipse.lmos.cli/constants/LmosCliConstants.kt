87ecfadc91d3a4890689d41c07e5a2249c4be8bc
bharatnpti <bharatnpti@gmail.com>
2025-03-03 15:19:28 +0530

    hardcoded path fix

--- src/main/kotlin/org.eclipse.lmos.cli/constants/LmosCliConstants.kt
+++ src/main/kotlin/org.eclipse.lmos.cli/constants/LmosCliConstants.kt
@@ -5,18 +5,24 @@ import java.nio.file.Path
 
 object LmosCliConstants {
 
-    val projectDir: Path = Path.of(System.getProperty("user.home")).resolve(".lmos").resolve("cli")
+    val projectDir: Path
+        get() = Path.of(System.getProperty("user.home")).resolve(".lmos").resolve("cli")
 
     object AgentStarterConstants {
         const val PACKAGE_NAME = "org.eclipse.lmos.starter"
-        val AGENT_DIRECTORY: Path = projectDir.resolve("agents")
-        val AGENT_PROJECTS_DIRECTORY: Path = AGENT_DIRECTORY.resolve("projects")
-        val AGENTS_REGISTRY: Path = AGENT_DIRECTORY.resolve("registry")
+        val AGENT_DIRECTORY: Path
+            get() = projectDir.resolve("agents")
+        val AGENT_PROJECTS_DIRECTORY: Path
+            get() = AGENT_DIRECTORY.resolve("projects")
+        val AGENTS_REGISTRY: Path
+            get() = AGENT_DIRECTORY.resolve("registry")
     }
 
     object CredentialManagerConstants {
-        val CREDENTIAL_DIRECTORY: Path = projectDir.resolve(".cred")
-        val CREDENTIAL_CONFIG: File = CREDENTIAL_DIRECTORY.resolve("credentials.yaml").toFile()
+        val CREDENTIAL_DIRECTORY: Path
+            get() = projectDir.resolve(".cred")
+        val CREDENTIAL_CONFIG: File
+            get() = CREDENTIAL_DIRECTORY.resolve("credentials.yaml").toFile()
     }
 
     const val PREFIX = "LLM_CONFIG"
