4c78182a3f1b01c69e457b89154a80c10fa05a7e
Patrick Schwager <patrick.schwager@telekom.de>
2025-04-29 12:52:26 +0200

    Use variable for kotlinx serialization version

--- lmos-runtime-service/build.gradle.kts
+++ lmos-runtime-service/build.gradle.kts
@@ -35,11 +35,12 @@ dependencies {
 // Can be omitted again when spring boot has upgraded to more recent kotlinx-serialization version.
 dependencyManagement {
     dependencies {
-        dependency("org.jetbrains.kotlinx:kotlinx-serialization-bom:1.8.0")
-        dependency("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
-        dependency("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.8.0")
-        dependency("org.jetbrains.kotlinx:kotlinx-serialization-core:1.8.0")
-        dependency("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:1.8.0")
+        val kotlinxSerializationVersion = "1.8.0"
+        dependency("org.jetbrains.kotlinx:kotlinx-serialization-bom:$kotlinxSerializationVersion")
+        dependency("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
+        dependency("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:$kotlinxSerializationVersion")
+        dependency("org.jetbrains.kotlinx:kotlinx-serialization-core:$kotlinxSerializationVersion")
+        dependency("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:$kotlinxSerializationVersion")
     }
 }
 
