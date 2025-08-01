c76e5e4a44c30013a5e295434897102f4ba94f49
Stefan Bußweiler <stefan.bussweiler@telekom.de>
2025-08-01 14:40:12 +0200

    Added copy rights comment.

--- lmos-runtime-core/src/main/kotlin/org/eclipse/lmos/runtime/core/service/outbound/AgentClassifierService.kt
+++ lmos-runtime-core/src/main/kotlin/org/eclipse/lmos/runtime/core/service/outbound/AgentClassifierService.kt
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.eclipse.lmos.runtime.core.service.outbound
 
 import org.eclipse.lmos.classifier.core.ClassificationResult
--- lmos-runtime-core/src/main/kotlin/org/eclipse/lmos/runtime/outbound/LmosAgentClassifierService.kt
+++ lmos-runtime-core/src/main/kotlin/org/eclipse/lmos/runtime/outbound/LmosAgentClassifierService.kt
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.eclipse.lmos.runtime.outbound
 
 import kotlinx.coroutines.Dispatchers
