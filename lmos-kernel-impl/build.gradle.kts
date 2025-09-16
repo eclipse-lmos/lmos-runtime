70e2f7eb13e1adf217b1aa38d03abcf37a8d70de
Robert Winkler <robwin@t-online.de>
2025-09-16 09:54:13 +0200

    Fix USE issues

--- lmos-kernel-impl/build.gradle.kts
+++ lmos-kernel-impl/build.gradle.kts
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 dependencies {
 
     implementation(project(":lmos-kernel"))
--- lmos-kernel/build.gradle.kts
+++ lmos-kernel/build.gradle.kts
@@ -1,3 +1,10 @@
+/*
+* SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
+
+
 dependencies {
     api("org.slf4j:slf4j-api:2.0.17")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
